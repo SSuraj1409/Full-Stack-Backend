e7fc602a7dc358bc01422bdd97787eecfb738626
SSuraj1409 <ssuraj1409@gmail.com>
2025-11-29 11:15:49 +0400

    update2

--- server.js
+++ server.js
@@ -7,3 +7,19 @@ app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
 });
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+});
+
+app.use("/images", express.static(path.join(__dirname, "images"), { fallthrough: false }));
+app.use((err, req, res, next) => {
+    if (err) return res.status(404).send({ error: "Image not found" });
+    next();
+});
